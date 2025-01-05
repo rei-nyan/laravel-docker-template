26f2860a854089e3a3320bf8ff418b29c4c6d4f2
IchikawaRei <ny11as125@gmail.com>
2025-01-05 15:28:01 +0900

    add: DBに新規作成される登録処理

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -22,5 +22,21 @@ class TodoController extends Controller
         TODO::all();
         return view('resources.todo.create'); // 追記
     }
+
+    public function store(Request $request) // 追記
+    {
+        $content = $request->input('content'); // 追記
+
+        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        $todo = new Todo();
+        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->content = $content;
+        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->save();
+
+        return redirect()->route('todo.index'); // 追記
+    
+        // dd($content);
+    }
     
 }
