d6b8ea18a1f933e86abb72d4153ac57f0f8587e1
IchikawaRei <ny11as125@gmail.com>
2025-01-06 15:21:35 +0900

    feat: ToDo詳細表示機能の追加、リファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -8,10 +8,16 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('resources.todo.index', ['todos' => $todos]);
         // 修正
@@ -26,19 +32,20 @@ class TodoController extends Controller
     public function store(Request $request) // 追記
     {
         $inputs = $request->all();
-        dd($inputs); // 追記
 
-        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo = new Todo();
-        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo ->fill($inputs);
-        $todo->content = $inputs['content'];
-        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-        $todo->save();
+        $this->todo->fill($inputs); // 変更
+        $this->todo->save(); // 変更
 
         return redirect()->route('todo.index'); // 追記
     
         // dd($content);
+    }
+
+    public function show($id)
+    {
+        $todo = $this->todo->find($id);
+
+        return view('resources.todo.show', ['todo' => $todo]);
     }
     
 }
